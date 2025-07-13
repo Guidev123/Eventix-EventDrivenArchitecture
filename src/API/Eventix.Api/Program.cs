7c75e238b717bf60128f31bfa912e40e71bed838
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-07-12 23:50:53 -0300

    Event sender for EventStore

--- src/API/Eventix.Api/Program.cs
+++ src/API/Eventix.Api/Program.cs
@@ -9,12 +9,6 @@ var app = builder
     .Build()
     .UsePipeline(builder);
 
-app.MapGet("api/v1/event-sourcing/{id:guid}", async (Guid id, IEventSourcingRepository eve) =>
-{
-    var result = await eve.GetAllAsync(id);
-    return result.Any() ? Results.Ok(result) : Results.BadRequest();
-});
-
 app.Run();
 
 public partial class Program;
